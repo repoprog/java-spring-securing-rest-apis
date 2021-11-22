cdf173a3486047961010a3286120e31948080293
Remigiusz <repogplus@gmail.com>
2021-11-22 14:29:08 +0100

    Completed

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
@@ -1,4 +1,51 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity(name = "authorities")
 public class UserAuthority {
+
+    @Id
+    private UUID id;
+
+    @Column
+    private String authority;
+
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    @ManyToOne
+    User user;
+
+    public UserAuthority() {}
+
+
+    public UserAuthority(User user, String authority) {
+        this.id = UUID.randomUUID();
+        this.user = user;
+        this.authority = authority;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/io/jzheaux/springsecurity/userprofiles/UserProfilesApplication.java
+++ src/main/java/io/jzheaux/springsecurity/userprofiles/UserProfilesApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,32 +14,37 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @SpringBootApplication
-public class UserProfilesApplication {
-	@Bean
-	public TomcatConnectorCustomizer connectorCustomizer() {
-		return container -> container.setPort(8081);
-	}
+public class UserProfilesApplication implements UserDetailsService {
+    @Bean
+    public TomcatConnectorCustomizer connectorCustomizer() {
+        return container -> container.setPort(8081);
+    }
 
-	@RestController
-	static class UserController {
-		@GetMapping("/user/{username}/fullName")
-		Optional<String> read(@PathVariable("username") String username) {
-			switch(username) {
-				case "user":
-					return Optional.of("User Userson");
-				case "hasread":
-					return Optional.of("Has Read");
-				case "haswrite":
-					return Optional.of("Has Write");
-				case "admin":
-					return Optional.of("Admin Adminson");
-				default:
-					return Optional.empty();
-			}
-		}
-	}
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        throw new UsernameNotFoundException("no user");
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserProfilesApplication.class, args);
-	}
+    @RestController
+    static class UserController {
+        @GetMapping("/user/{username}/fullName")
+        Optional<String> read(@PathVariable("username") String username) {
+            switch (username) {
+                case "user":
+                    return Optional.of("User Userson");
+                case "hasread":
+                    return Optional.of("Has Read");
+                case "haswrite":
+                    return Optional.of("Has Write");
+                case "admin":
+                    return Optional.of("Admin Adminson");
+                default:
+                    return Optional.empty();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProfilesApplication.class, args);
+    }
 }
